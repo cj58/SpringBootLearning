c9a5b453afc1fccb8c4403127cf0481621d25f48
cj <545665592@qq.com>
2019-06-26 11:56:50 +0800

    spring boot admin server

---
+++ springboot-admin/adminserver/src/main/java/com/cj/springboot/SpringBootAdminApplication.java
@@ -0,0 +1,92 @@
+package com.cj.springboot;
+
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+/**
+ * @author wangqm-b
+ *
+ */
+@EnableAdminServer
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringBootAdminApplication {
+
+
+    @Profile("insecure")
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .anyRequest()
+                    .permitAll()
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringAntMatchers("/instances", "/actuator/**");
+        }
+    }
+
+    @Profile("secure")
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+            successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringAntMatchers("/instances", "/actuator/**");
+            http.csrf().disable();
+            // @formatter:on
+        }
+    }
+
+
+    private static ConfigurableApplicationContext ctx;
+
+    public static void main(String[] args) {
+        ctx = SpringApplication.run(SpringBootAdminApplication.class, args);
+
+
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//
+//        for (String beanName : beanNames) {
+//            System.out.println(beanName);
+//        }
+//        System.out.println("所有beanNames个数：" + beanNames.length);
+    }
+
+
+}
+
